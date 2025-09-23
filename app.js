599489d2cda8970e3dcb139175205bbdff6d0db1
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2025-09-23 19:46:10 +0300

    fix: delete jade

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.set("view engine", "jade");
+// app.set("view engine", "jade");
 
 app.use("/uploads", express.static("uploads"));
 
