5b41334947ad26572d73fd3696c94bed588daa3f
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2025-09-29 22:34:55 +0300

    feat: add routes, controller for book

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,83 +1,32 @@
+const { default: prisma } = require("../prisma/prisma-client");
 const db = require("../models");
 const Book = db.Book;
 const Author = db.Author;
 const Category = db.Category;
 const Rating = db.Rating;
 const Comment = db.Comment;
-// const Op = db.Sequelize.Op;
 const uuid = require("uuid");
 const path = require("path");
 
-// const getPagination = (page, size) => {
-//   const limit = size ? +size : 3;
-//   const offset = page ? page * limit : 0;
-
-//   return { limit, offset };
-// };
-
-// const getPagingData = (data, page, limit) => {
-//   const { count: totalItems, rows: books } = data;
-//   const currentPage = page ? +page : 0;
-//   const totalPages = Math.ceil(totalItems / limit);
-
-//   return { totalItems, books, totalPages, currentPage };
-// };
-
-// const pagBookfindAll = (req, res) => {
-//   const { page, size, title } = req.query;
-//   let condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
-
-//   const { limit, offset } = getPagination(page, size);
-
-//   Book.findAndCountAll({
-//     where: condition,
-//     limit,
-//     offset,
-//     include: [db.Author, db.Category, db.Rating],
-//     order: ["id"],
-//   })
-//     .then((data) => {
-//       const response = getPagingData(data, page, limit);
-//       res.json(response);
-//     })
-//     .catch((err) => {
-//       res.status(500).json({
-//         message: err.message || "Some error occurred while retrieving books.",
-//       });
-//     });
-// };
-
-// find all published Book
-// exports.findAllPublished = (req, res) => {
-//   const { page, size } = req.query;
-//   const { limit, offset } = getPagination(page, size);
-
-//   Book.findAndCountAll({ where: { published: true }, limit, offset })
-//     .then(data => {
-//       const response = getPagingData(data, page, limit);
-//       res.send(response);
-//     })
-//     .catch(err => {
-//       res.status(500).send({
-//         message:
-//           err.message || "Some error occurred while retrieving books."
-//       });
-//     });
-// };
-
 const createBook = async (req, res) => {
-  // сделать
   const { name, price, description } = req.body;
-  // const { img } = req.files;
-  // let fileName = uuid.v4() + ".jpg";
-  // img.mv(path.resolve(__dirname, "..", "static", fileName));
+  const { imgUrl } = req.files;
+
+  if (!name || !price || !description || !imgUrl) {
+    return res.status(400).json({ error: "Нужно заполнить все поля" });
+  }
+
+  let fileName = uuid.v4() + ".jpg";
+  imgUrl.mv(path.resolve(__dirname, "..", "uploads", fileName));
 
   try {
-    const book = await Book.create({
-      name,
-      price,
-      description,
-      // img: fileName,
+    const book = await prisma.book.create({
+      data: {
+        name,
+        price,
+        description,
+        imgUrl: fileName,
+      },
     });
 
     return res.status(201).json(book);
@@ -87,27 +36,7 @@ const createBook = async (req, res) => {
   }
 };
 
-// return
-// const deleteBook = async (req, res) => {
-//   // вместе с книгой удалять ее лайки, комменты и все, что с ней связано
-//   const id = req.params.id;
-
-//   try {
-//     const bookId = await Book.findOne({ where: { id } });
-//     if (!bookId) {
-//       return res.json({ error: "Книга с этим id не найдена" });
-//     }
-
-//     await Book.destroy({ where: { id } });
-
-//     return res.json({ message: "Книга успешно удалена" });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json(error);
-//   }
-// };
-
-const findBooks = async (req, res) => {
+const findAllBooks = async (req, res) => {
   let { AuthorId, CategoryId, page, limit } = req.query;
   page = page || 1;
   limit = limit || 4;
@@ -115,57 +44,14 @@ const findBooks = async (req, res) => {
   let books;
 
   try {
-    if (!AuthorId && !CategoryId) {
-      console.log("!AuthorId && !CategoryId");
-      books = await Book.findAndCountAll({
-        include: [Author, Category, Rating, Comment],
-        limit,
-        offset,
-      });
-    }
-
-    if (AuthorId && !CategoryId) {
-      console.log("AuthorId && !CategoryId");
-      books = await Book.findAndCountAll({
-        include: [{ model: Author, where: { id: AuthorId } }, Category, Rating],
-        limit,
-        offset,
-      });
-    }
-
-    if (!AuthorId && CategoryId) {
-      console.log("!AuthorId && CategoryId");
-      books = await Book.findAndCountAll({
-        include: [
-          { model: Category, where: { id: CategoryId } },
-          Author,
-          Rating,
-        ],
-        limit,
-        offset,
-      });
-    }
-
-    if (AuthorId && CategoryId) {
-      console.log("AuthorId && CategoryId");
-      books = await Book.findAndCountAll({
-        include: [
-          { model: Author, where: { id: AuthorId } },
-          { model: Category, where: { id: CategoryId } },
-          Rating,
-        ],
-        limit,
-        offset,
-      });
-    }
-    return res.json(books.rows);
+    return res.json(books);
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
   }
 };
 
-const findBook = async (req, res) => {
+const findBookById = async (req, res) => {
   const id = req.params.id;
 
   try {
@@ -187,7 +73,7 @@ const findBook = async (req, res) => {
   }
 };
 
-const putBook = async (req, res) => {
+const putBookById = async (req, res) => {
   const id = req.params.id;
   const { name, price, description, img } = req.body;
 
@@ -199,7 +85,6 @@ const putBook = async (req, res) => {
 
     await Book.update(
       {
-        // || undefined
         name,
         price,
         description,
@@ -215,11 +100,29 @@ const putBook = async (req, res) => {
   }
 };
 
+const deleteBook = async (req, res) => {
+  // вместе с книгой удалять ее лайки, комменты и все, что с ней связано
+  const id = req.params.id;
+
+  try {
+    const bookId = await Book.findOne({ where: { id } });
+    if (!bookId) {
+      return res.json({ error: "Книга с этим id не найдена" });
+    }
+
+    await Book.destroy({ where: { id } });
+
+    return res.json({ message: "Книга успешно удалена" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
 module.exports = {
   createBook,
   deleteBook,
-  findBooks,
-  findBook,
-  putBook,
-  // pagBookfindAll,
+  findAllBooks,
+  findBookById,
+  putBookById,
 };
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const userController = require("../controllers/userController");
+const bookController = require("../controllers/bookController");
 const router = express.Router();
 const multer = require("multer");
 const authMiddleware = require("../middleware/authMiddleware");
@@ -17,8 +18,14 @@ const uploads = multer({ storage: storage });
 
 router.post("/api/v1/register", userController.registration);
 router.post("/api/v1/login", userController.login);
-router.get("/api/v1/user:id", authMiddleware, userController.getUserById);
+router.get("/api/v1/users:id", authMiddleware, userController.getUserById);
 router.get("/api/v1/current", authMiddleware, userController.currentUser);
-router.put("/api/v1/user:id", authMiddleware, userController.updateUser);
+router.put("/api/v1/users:id", authMiddleware, userController.updateUser);
+
+router.post("/api/v1/books", bookController.createBook); // add check admin
+router.get("/api/v1/books", bookController.findAllBooks);
+router.get("/api/v1/books/:id", bookController.findBookById);
+router.delete("/api/v1/books/:id", bookController.deleteBook); // add check admin
+router.put("/api/v1/books/:id", bookController.putBookById); // add check admin
 
 module.exports = router;
