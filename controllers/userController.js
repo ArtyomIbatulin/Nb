146d433cbbfce9ed7249a4781e5fdf6b88b54994
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2026-01-15 17:25:26 +0300

    fix: change avatarUrl, avatarPath, png in the userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,9 +21,9 @@ const registration = async (req, res) => {
 
     const hashPassword = await bcrypt.hash(password, 5);
 
-    const png = Jdenticon.toPng(name, 200);
+    const png = Jdenticon.toPng(`${name}${Date.now()}`, 200);
     const avatarName = `${name}_${Date.now()}.png`;
-    const avatarPath = path.join(__dirname, "../uploads", avatarName);
+    const avatarPath = path.join(__dirname, "/../uploads", avatarName);
     fs.writeFileSync(avatarPath, png);
 
     const user = await prisma.user.create({
@@ -32,7 +32,7 @@ const registration = async (req, res) => {
         password: hashPassword,
         role,
         name,
-        avatarUrl: `/uploads/${avatarPath}`,
+        avatarUrl: `/uploads/${avatarName}`,
       },
     });
 
