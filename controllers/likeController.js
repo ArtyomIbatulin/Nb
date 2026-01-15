1318b270d567625a0456022dc8ec8e5492bf896d
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2026-01-15 16:45:46 +0300

    feat: add likePost in the likeController

---
+++ controllers/likeController.js
@@ -0,0 +1,41 @@
+const { default: prisma } = require("../prisma/prisma-client");
+
+const likePost = async (req, res) => {
+  const { postId } = req.params;
+  const userId = req.user.userId;
+
+  try {
+    if (!postId) {
+      return res.status(400).json({ message: "Все поля обязательны" });
+    }
+
+    const existingLike = await prisma.like.findFirst({
+      where: { postId, userId },
+    });
+    if (existingLike) {
+      return res.status(400).json({ message: "Вы уже ставили лайк" });
+    }
+
+    const like = await prisma.like.create({
+      data: {
+        userId,
+        postId,
+      },
+    });
+
+    return res.json(like);
+  } catch (error) {
+    console.log(error, "error: likePost");
+    return res.status(500).json(error);
+  }
+};
+
+const unlikePost = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error, "error: unlikePost");
+    return res.status(500).json(error);
+  }
+};
+
+module.exports = { likePost, unlikePost };
