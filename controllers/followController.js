0d5e5b21f587e19252b0471f042c75921ae57be6
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2026-01-18 18:33:45 +0300

    feat: add unfollowUser to the followController

--- controllers/followController.js
+++ controllers/followController.js
@@ -35,7 +35,25 @@ const followUser = async (req, res) => {
 };
 
 const unfollowUser = async (req, res) => {
+  const { followingId } = req.body;
+  const userId = req.user.userId;
+
   try {
+    const follows = await prisma.follows.findFirst({
+      where: {
+        AND: [{ followerId: userId }, { followingId }],
+      },
+    });
+
+    if (!follows) {
+      return res.status(404).json({ message: "Такой записи нет" });
+    }
+
+    await prisma.follows.delete({
+      where: { id: follows.id },
+    });
+
+    return res.status(200).json({ message: "Вы отписались" });
   } catch (error) {
     console.log(error, "error: unfollowUser");
     return res.status(500).json(error);
